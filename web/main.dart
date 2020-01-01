ed0b606b65d9cf24bb7cb039e24db166ebcf1df0
Pavel Zinovev <ShinaZin@mail.ru>
2020-01-02 01:33:14 +0300

    Added react support

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,16 @@
 import 'dart:html';
 
+import 'package:react/react_client.dart' as ReactClient;
+import 'package:react/react.dart';
+import 'package:react/react_dom.dart' as ReactDOM;
+
+class AppComponent extends Component2 {
+  render() => h1({}, 'Hello, name');  
+}
+
+
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  ReactClient.setClientConfiguration();
+  var App = registerComponent2(() => new AppComponent());
+  ReactDOM.render(App({}), querySelector('#react_root'));
 }
